1a02d8bef7dd3f7a4230e3ec0bb92b25a4c139d9
mgigli16 <mgigli16@g.holycross.edu>
2016-02-16 10:51:31 -0500

    Commit number 2 just added print line

--- Common Program/ViewController.swift
+++ Common Program/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("I can't wait to push this project to Github")
+        print("Test that I have updated successfully")
     }
 
     override func didReceiveMemoryWarning() {
