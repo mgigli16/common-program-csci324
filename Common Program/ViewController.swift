924fb1bdb23384e0cd12d776b16574cd05e311e3
mgigli16 <mgigli16@g.holycross.edu>
2016-02-16 09:14:01 -0500

    Initial Commit

---
+++ Common Program/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Common Program
+//
+//  Created by Matt Gigliotti on 2/16/16.
+//  Copyright © 2016 Matt Gigliotti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Common ProgramTests/Common_ProgramTests.swift
@@ -0,0 +1,36 @@
+//
+//  Common_ProgramTests.swift
+//  Common ProgramTests
+//
+//  Created by Matt Gigliotti on 2/16/16.
+//  Copyright © 2016 Matt Gigliotti. All rights reserved.
+//
+
+import XCTest
+@testable import Common_Program
+
+class Common_ProgramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
