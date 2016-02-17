14c5c0426cad64fbb6c26b3716c5b3ac4c05c0e7
mgigli16 <mgigli16@g.holycross.edu>
2016-02-17 15:31:18 -0500

    Have added a basic interface.

--- Common Program/ViewController.swift
+++ Common Program/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var words: Set<String> = ["Dog", "Cat", "Horse", "People"]
 
+    @IBOutlet weak var Label: UILabel!
+    
+    @IBOutlet weak var UserText: UITextField!
+    
+    @IBAction func Check(sender: AnyObject) {
+        
+        if words.contains(UserText.text!) {
+            Progress.text = "Word was Found"
+        }
+        else {
+            Progress.text = "Word was not Found"
+        }
+    
+    }
+    
+    @IBAction func AddWord(sender: AnyObject) {
+        words.insert(UserText.text!)
+        Progress.text = "Word was Successfully added"
+    }
+    
+    @IBOutlet weak var Progress: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("I can't wait to push this project to Github")
-        print("Test that I have updated successfully")
+        //print("I can't wait to push this project to Github")
+        //print("Test that I have updated successfully")
     }
 
     override func didReceiveMemoryWarning() {
