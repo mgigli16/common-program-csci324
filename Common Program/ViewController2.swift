79b98871f0895d7db48df5900c21fa7937adb430
mgigli16 <mgigli16@g.holycross.edu>
2016-02-17 16:55:20 -0500

    Added link between view controllers

---
+++ Common Program/ViewController2.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController2.swift
+//  Common Program
+//
+//  Created by Matt Gigliotti on 2/16/16.
+//  Copyright © 2016 Matt Gigliotti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //print("I can't wait to push this project to Github")
+        //print("Test that I have updated successfully")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
