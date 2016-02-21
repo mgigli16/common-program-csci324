761e5ea62319bdbc58643d272bd4e08c5bf6df1d
mgigli16 <mgigli16@g.holycross.edu>
2016-02-21 16:56:25 -0500

    black tab controller with commented out import code

--- Common Program/ViewController2.swift
+++ Common Program/ViewController2.swift
@@ -10,27 +10,37 @@ import UIKit
 
 class ViewController2: UIViewController {
     
+    var words = Set<String>()
+    
     @IBOutlet weak var Instructions: UILabel!
     @IBOutlet weak var OpenMessage: UILabel!
     @IBOutlet weak var PathField: UITextField!
     @IBOutlet weak var Output: UILabel!
     @IBAction func SubmitPath(sender: AnyObject) {
-        Output.text = " "
-        var path = PathField.text
-        Output.text = path
-
-    }
+        /*
+        var path = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("dictionary", ofType: "txt")!)
+        var possibleContent = String.stringWithContentsOfFile(path, encoding: NSUTF8StringEncoding)
+        
+        if let content = possibleContent {
+            var array = content.componentsSeparatedByString("\n")
+            
+            for text in array {
+                println(content)
+            }
+        }
+*/
+}
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-    }
+}
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
+}
     
 }
+
 
